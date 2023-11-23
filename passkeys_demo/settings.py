563c8221442463ce299326150c1b58b1582ba8c3
Nicolai Stein <nicolai_stein@outlook.de>
2023-11-23 17:29:59 +0100

    Added login and profile page, routing

--- passkeys_demo/settings.py
+++ passkeys_demo/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ID = env('PROJECT_ID')
+API_SECRET = env('API_SECRET')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- passkeys_demo/views.py
+++ passkeys_demo/views.py
@@ -1,11 +1,57 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import redirect
+import requests
+from jose import jwt
+from passkeys_demo.settings import PROJECT_ID, API_SECRET
+
+# Session config
+short_session_cookie_name = "cbo_short_session"
+issuer = f"https://{PROJECT_ID}.frontendapi.corbado.io"
+jwks_uri = f"https://{PROJECT_ID}.frontendapi.corbado.io/.well-known/jwks"
 
 
 def index(request):
-    context = {}
+    context = {
+        'PROJECT_ID': PROJECT_ID,
+    }
     return render(request, "index.html", context)
 
 def profile(request):
-    return HttpResponse("Profile page")
+    token = request.COOKIES.get(short_session_cookie_name)
+
+    try:
+        if not token:
+            raise ValueError("No token found")
+
+        response = requests.get(jwks_uri)
+        response.raise_for_status()
+
+        jwks = response.json()
+        public_key = jwks["keys"][0]
+
+        payload = jwt.decode(
+            token,
+            key=public_key,
+            algorithms=["RS256"],
+            audience=API_SECRET,
+            issuer=issuer,
+        )
+
+        if payload["iss"] != issuer:
+            raise ValueError("Invalid issuer")
+
+        context = {
+            'PROJECT_ID': PROJECT_ID,
+            'USER_ID': payload["sub"],
+            'USER_NAME': payload.get("name"),
+            'USER_EMAIL': payload.get("email"),
+         }
+
+        return render(request, "profile.html", context)
+    
+    except Exception as e:
+        print(e)
+        return redirect("/")
+
